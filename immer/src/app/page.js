e83b5ea432dff95495c15ac46ae405bcf05a896a
Tomas <silventetomas@gmail.com>
2023-08-17 00:29:53 -0300

    NavBar component created

--- immer/src/app/page.js
+++ immer/src/app/page.js
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 import BlogCta from '@/components/BlogCta'
 import Footer from '@/components/Footer'
+import { NavBar } from '@/components/NavBar'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between bg-white">
+      <NavBar />
       <BlogCta />
       <Footer />
 
