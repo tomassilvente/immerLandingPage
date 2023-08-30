130de4a4fa5900cba5f92e342aed1edf634688b7
Tomas <silventetomas@gmail.com>
2023-08-30 13:58:58 -0300

    SignIn, SingUp pages made, assets added

--- immer/src/app/signup/page.jsx
+++ immer/src/app/signup/page.jsx
@@ -27,7 +27,7 @@ function Page() {
     return router.push("/adminblog");
   };
   return (
-    <div className="flex h-[975px] bg-white">
+    <div className="flex h-screen bg-white">
       <div className="lg:w-[50%] sm:w-[80%] w-0 grid content-center place-items-center"
       style={{
         backgroundImage: `url(${SignInImg})`,
@@ -39,12 +39,12 @@ function Page() {
         <Image className="invisible sm:visible" src={'/assets/sign/immer_logo_white.svg'} width={400} height={1} />
       </div>
       <div className="sm:p-10 xl:p-20 grid-rows-3 md:w-[60%]">
-      <h1 className="text-2xl ml-20 text-center text-primary mt-20 sm:mt-0 sm:invisible">Welcome to</h1>
+      <h1 className="text-2xl ml-20 text-center text-primary mt-20 sm:mt-0 sm:text-[0px]">Welcome to</h1>
       <Image className="ml-[170px] mt-2 sm:m-0 sm:w-0" src={'/assets/blog/immer_logo.svg'} width={250} height={1} />
         <div className="mt-20 sm:mt-0 ml-20 sm:ml-0 lg:mx-20 mb-30">
-        <Image className="mb-8 mt-20 sm:w-[120px] w-0" src={'/assets/blog/immer_logo.svg'} width={250} height={1} />
+        <Image className="mb-8 mt-12 sm:w-[120px] w-0" src={'/assets/blog/immer_logo.svg'} width={250} height={1} />
         <Image 
-          className=" 2xl:mx-[35%]  lg:ml-[23%] md:ml-[20%] sm:ml-[10%]  mb-20 mt-20 ml-32  " 
+          className=" 2xl:mx-[35%] ml-[25%] w-[50%]  mb-20   " 
           src={'/assets/sign/success.svg'} 
           width={200} height={1} 
           style={{
@@ -54,7 +54,7 @@ function Page() {
               : "none"
           }}/>
           <p 
-            className='ml-16 sm:ml-0 text-center text-black font-light mb-20 text-xl '
+            className='ml-10 sm:ml-0 text-center text-black font-light mb-20 text-xl '
             style={{
               display:
                 success
@@ -117,7 +117,7 @@ function Page() {
           </form>
           
           <button 
-              className="ml-10 sm:ml-0 row-start-3 bg-primary text-white mt-8 py-3 w-[100%] rounded-[8px]" 
+              className="ml-[5%] sm:ml-0 row-start-3 bg-primary text-white mt-8 py-3 w-[100%] rounded-[8px]" 
               type="submit"
               style={{
                 display:
