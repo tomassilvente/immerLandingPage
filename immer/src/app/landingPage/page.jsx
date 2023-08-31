a734806f96faa277d042eff81d0b75625109c1e8
cheronodaisy <jepchumbadaisy96@gmail.com>
2023-08-31 12:10:31 +0300

    landing page file

---
+++ immer/src/app/landingPage/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import AboutSection from '@/components/landingPageComponents/aboutSection';
+
+const LandingPage = () => {
+
+  return (
+    <div>
+      <AboutSection />
+    </div>
+  )
+}
+  
+export default LandingPage
