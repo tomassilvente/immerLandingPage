552fd4aaeebca5557fe879a6336255d204350ccc
Tomas <silventetomas@gmail.com>
2023-10-05 14:53:22 -0300

    Modified: admin/blogs

--- src/app/admin/blogs/createnew/page.jsx
+++ src/app/admin/blogs/createnew/page.jsx
@@ -1,88 +1,197 @@
 "use client";
 
+import {  getAuth } from "@firebase/auth";
+
 import React, { useState } from "react";
-import addData from "@/firebase/firestore/addData";
-import { uploadImage } from "@/firebase/firestore/uploadImage";
+
+import addData from "../../../../firebase/firestore/addData";
+
+import { uploadImage } from "../../../../firebase/firestore/uploadImage";
+
+ 
 
 function CreateNew() {
+
   const [title, setTitle] = useState("");
+
   const [description, setDescription] = useState("");
+
   const [imageUrl, setImageUrl] = useState(null);
+
   const [image, setImage] = useState(null);
+
+ 
 
   const handleForm = async () => {
 
+ 
+
     console.log("Selected image:", image);
 
+    
+
+    // Getting User from Database
+
+    let user = getAuth().currentUser //.displayName
+
+    let profilePic = getAuth().currentUser //.photoURL
+
+ 
+
+    //While we don't have users working
+
+    if(!user){
+
+      user = "Random Profile"
+
+      profilePic = "../../../../assets/blog/m1.png"
+
+    }
+
+ 
+
     // Create the data object with title and description
+
     const data = {
+
+      user: user,
+
+      profilePic: profilePic,
+
       title: title,
+
       description: description,
+
       image: image,
+
       imageUrl: imageUrl,
+
     };
 
+ 
+
     try {
+
       if (image) {
+
         // Upload the image to Firebase Storage and get its URL
+
         const imageURL = await uploadImage(image);
 
         setImageUrl(imageURL);
+
       }
 
-      // Add the data to the Firestore collection
-      await addData("blogs", null, data);
  
+
+      // Add the data to the Firestore collection
+
+      await addData("blogs", null, data);
+
+ 
+
       // Reset the form
+
       setTitle("");
+
       setDescription("");
+
       setImage(null);
+
       console.log(data)
+
     } catch (error) {
+
       console.error("Error uploading data:", error);
+
     }
+
   };
 
+ 
+
   return (
+
     <div className="p-4">
+
       <h2 className="text-2xl font-bold mb-4">Create New Blog</h2>
+
       <div className="flex flex-col space-y-2">
+
         <label htmlFor="title">title:</label>
+
         <input
+
           type="text"
+
           id="title"
+
           value={title}
+
           onChange={(e) => setTitle(e.target.value)}
+
           className="border border-gray-300 p-2"
+
         />
+
       </div>
+
       <div className="flex flex-col space-y-2">
+
         <label htmlFor="description">description:</label>
+
         <input
+
           type="text"
+
           id="description"
+
           value={description}
+
           onChange={(e) => setDescription(e.target.value)}
+
           className="border border-gray-300 p-2"
+
         />
+
       </div>
+
       <div className="flex flex-col space-y-2">
+
         <label htmlFor="image">Image:</label>
+
         <input
+
           type="file"
+
           id="image"
+
           onChange={(e) => setImage(e.target.files[0])}
+
           className="border border-gray-300 p-2"
+
         />
+
       </div>
+
       <button
+
         onClick={handleForm}
+
         className="bg-blue-500 text-white p-2 rounded hover:bg-blue-600 mt-4"
+
       >
+
         Submit
+
       </button>
+
     </div>
+
   );
+
 }
+
+ 
 
 export default CreateNew;
--- src/app/admin/blogs/page.jsx
+++ src/app/admin/blogs/page.jsx
@@ -1,71 +1,175 @@
 "use client";
 
+ 
+
 import React, { useState, useEffect } from "react";
-import getData from "@/firebase/firestore/getData";
-import deleteBlog from "@/firebase/firestore/deleteDoc";
+
+import getData from "../../../firebase/firestore/getData";
+
+import deleteBlog from "../../../firebase/firestore/deleteDoc";
+
+ 
 
 function Blogs() {
+
   const [documentData, setDocumentData] = useState([]);
 
+ 
+
   useEffect(() => {
+
     // Fetch a specific document from Firestore and set it to the state
+
     const fetchData = async () => {
+
       try {
+
         const { result, error } = await getData("blogs", null);
 
+ 
+
         if (error) {
+
           console.error("Error fetching document:", error);
+
         } else {
+
           if (Array.isArray(result)) {
+
             setDocumentData(result);
+
           } else {
+
             console.error("Document data is not valid:", result);
+
           }
+
         }
+
       } catch (error) {
+
         console.error("Error fetching document:", error);
+
       }
+
     };
 
+ 
+
     fetchData();
+
   }, []);
 
+ 
+
   const handleDelete = async (id) => {
+
     const wasDeleted = await deleteBlog(id);
+
     if (wasDeleted) {
+
       setDocumentData((prevBlogs) =>
+
         prevBlogs.filter((blog) => blog.id !== id)
+
       );
+
     }
+
   };
 
+ 
+
   return (
+
     <div className="p-4">
-      <h2 className="text-2xl font-bold mb-4">
-        Here you can find all the blogs uploaded and delete them
+
+      <h2 className="text-2xl text-center font-bold my-5">
+
+        Admin Blog
+
       </h2>
+
       {documentData.length > 0 ? (
+
         documentData.map((blog) => (
-          <div
-            key={blog.id}
-            className="border border-gray-300 p-4 rounded shadow mb-4"
-          >
-            <h3 className="text-lg font-bold mb-2">{blog.title}</h3>
-            <p>{blog.description}</p>
-            <img src={blog.imageUrl} alt={blog.title} />
-            <button
-              onClick={() => handleDelete(blog.id)}
-              className="mt-4 bg-red-500 text-white p-2 rounded hover:bg-red-600"
-            >
-              Delete
-            </button>
+
+          <div className="md:w-[60%] md:ml-[20%] flex flex-col border-primary border rounded-lg my-6 p-7" key={blog.id}>
+
+          <div className="grid grid-cols-3">
+
+            <div className="flex col-start-1 col-end-3">
+
+              <img className="h-[35px] w-[35px] rounded-full mt-1 mr-2" src={blog.profilePic}/>
+
+              <h1 className="text-xl font-bold m-2">{blog.user}</h1>
+
+            </div>
+
+              <button
+
+                onClick={() => handleDelete(blog.id)}
+
+                className="col-start-3 ml-[50%] max-h-[40px] w-[50%] bg-red-500 text-white p-2 rounded hover:bg-red-600"
+
+                >
+
+                Delete
+
+              </button>
+
           </div>
+
+          <div className="my-5 md:w-[43vw] h-3 bg-gradient-to-r from-[#FF6C00] to-[#FF6C0000]"></div>
+
+          <div className="flex md:grid lg:flex justify-items-center ">
+
+            {
+
+              blog.description
+
+              ? (
+
+                <p className="text-[#555555] text-justify font-normal text-sm xl:text-base mr-4 md:mr-0  lg:mr-7 leading-normal">
+
+                  {blog.description}
+
+                </p>
+
+                )
+
+              : <></>
+
+            }
+
+            {
+
+              blog.imageUrl
+
+              ? (<img className="rounded-md max-h-[300px] max-w-[300px] md:mt-5 lg:mt-0" src={blog.imageUrl} alt={blog.title} />)
+
+              : (<></>)
+
+            }
+
+          </div>
+
+        </div>
+
         ))
+
       ) : (
+
         <p>No blogs found.</p>
+
       )}
+
     </div>
+
   );
+
 }
+
+ 
 
 export default Blogs;
