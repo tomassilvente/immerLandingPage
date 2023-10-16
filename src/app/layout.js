ed950278c7caf1ad58d65123f405be2f675153af
Emanuel Manrique <manriqueemanuel@gmail.com>
2023-10-16 11:53:54 -0300

    fix: withaut error

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import { AuthProvider } from "@/context/AuthContext";
+import { AuthProvider } from "@/app/context/AuthContext";
 import "@/firebase/config";
 
 const inter = Inter({ subsets: ["latin"] });
