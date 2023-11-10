d585eace842ece1411513adcb97045d5ddc647c8
Tomas <silventetomas@gmail.com>
2023-11-10 15:01:27 -0300

    [FIX] Again

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -102,7 +102,7 @@ function Page() {
         iconHeight={180}
       />
       <div className="xl:max-w-5xl md:max-w-3xl max-w-xl pt-16 mx-auto mt-14">
-        <article className="lg:max-h-[1000px] max-h-[600px] p-2 dark:bg-gray-800 dark:text-gray-50 m-2">
+        <article className=" p-2 dark:bg-gray-800 dark:text-gray-50 m-2">
           <div className="m-5">
             <h1 className="text-4xl text-center font-bold ">
               {documentData.title}
